d1f2db044a5672a404963c90fc25383b6b199134
Francisco Ramírez <TUEMAIL@gmail.com>
2026-05-17 02:22:17 -0400

    chore: create api gateway base project

---
+++ src/main/java/com/duoc/reservams/apigateway/ReservamsApiGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.duoc.reservams.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservamsApiGatewayApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ReservamsApiGatewayApplication.class, args);
+	}
+
+}
